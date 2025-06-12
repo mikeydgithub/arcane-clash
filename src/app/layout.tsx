b342709754a374f8ca49d92707cf48bccb08628c
Michael Diamond <ampgzo2@gmail.com>
2025-06-12 18:02:35 +0000

    there is also this appearing in the console. 6000-firebase-studio-17480

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -6,11 +7,13 @@ import { Toaster } from "@/components/ui/toaster";
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
+  display: 'swap', // Added display: 'swap'
 });
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+  display: 'swap', // Added display: 'swap'
 });
 
 export const metadata: Metadata = {
