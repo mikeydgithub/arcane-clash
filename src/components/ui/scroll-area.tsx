5191ac9c1ad42aecb2e52a00d7feb281a1f4973e
Michael Diamond <ampgzo2@gmail.com>
2026-02-12 19:11:16 +0000

    This looks great. I would like to see what it would like with suggestion

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -40,7 +41,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className={cn("relative flex-1 rounded-sm bg-accent text-accent indicator-active")} />
+    <ScrollAreaPrimitive.ScrollAreaThumb className={cn("relative flex-1 rounded-sm animate-energy-stream")} />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
